b32c9572ef648a26061bf2d6cd72f0a61bc072c5
alaminhossain1997web-tec <alaminhossain1997.web@gmail.com>
2026-07-31 04:23:46 +0600

    backend host link included

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://taskcreateapp.netlify.app",
   credentials: true,
 }));
 app.use(cookieParser());
